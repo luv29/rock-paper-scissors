b5c3bdfe5266e595b53f1c1c8395f80479081005
luv29 <luvkansal29@gmail.com>
2024-04-14 18:53:33 +0530

    sime minor touchups.

--- script.js
+++ script.js
@@ -37,31 +37,3 @@ Array.from(btns.children).forEach(button=>{
     })
 })
 
-function playGame() {
-    let playerScore=0;
-    let computerScore=0;
-    for(let i=0;i<5;i++) {
-        let playerSelection = prompt("Ebter your choice. Rock, Paper or Scissor");
-        playerSelection = playerSelection.toLowerCase();
-
-        let computerSelection = getComputerChoice();
-        
-        if(playerSelection == computerSelection)
-            console.log("It's a Tie! Both chode "+playerSelection+".");
-        else if( (playerSelection=="rock" && computerSelection=="scissor") || (playerSelection=="paper" && computerSelection=="rock") || (playerSelection=="scissor" && computerSelection=="paper") ) {
-            console.log("You Won! "+playerSelection + " beats " + computerSelection + ".");
-            playerScore++;
-        }
-        else {
-            console.log("You Lose! "+ computerSelection + " beats " + playerSelection);
-            computerScore++;
-        }
-    }
-
-    if(playerScore == computerScore)
-        console.log("It's a Tie. Both Player and Computer scored " + playerScore + ".");
-    else if(playerScore > computerScore)
-        console.log("You Won. Your Score " + playerScore + ", compuer Score " + computerScore + ".");
-    else
-        console.log("You Lose. Your Score " + playerScore + ", compuer Score " + computerScore + ".");
-}
