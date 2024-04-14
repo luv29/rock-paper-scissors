4fd8ee279653097633c5d17cd148f277661c767e
luv29 <luvkansal29@gmail.com>
2024-04-14 18:44:05 +0530

    Added UI to the project.

--- script.js
+++ script.js
@@ -5,14 +5,37 @@ function getComputerChoice() {
     else return "scissor";
 }
 
+let ps=0;
+let cs=0;
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection)
         return "It's a Tie! Both chode "+playerSelection+".";
-    else if( (playerSelection=="rock" && computerSelection=="scissor") || (playerSelection=="paper" && computerSelection=="rock") || (playerSelection=="scissor" && computerSelection=="paper") )
+    else if( (playerSelection=="rock" && computerSelection=="scissor") || (playerSelection=="paper" && computerSelection=="rock") || (playerSelection=="scissor" && computerSelection=="paper") ) {
+        ps++;
         return "You Won! "+playerSelection + " beats " + computerSelection + ".";
-    else
+    }
+    else {
+        cs++;
         return "You Lose! "+ computerSelection + " beats " + playerSelection;
+    }
 }
+
+let para = document.createElement("p");
+para.style.textAlign = "center";
+para.style.fontSize="30px";
+para.style.fontWeight="bold";
+document.body.appendChild(para);
+
+let btns = document.querySelector(".buttons");
+
+Array.from(btns.children).forEach(button=>{
+    button.addEventListener("click", e=> {
+        let computerSelection = getComputerChoice();
+        let message = playRound(button.id, computerSelection);
+
+        para.textContent = message;
+    })
+})
 
 function playGame() {
     let playerScore=0;
@@ -42,5 +65,3 @@ function playGame() {
     else
         console.log("You Lose. Your Score " + playerScore + ", compuer Score " + computerScore + ".");
 }
-
-playGame();
